b2f343b617fffa90fa695c83b3a3a459ba1f86a8
ViktorTap <ab4380@student.jamk.fi>
2022-08-21 10:27:01 +0300

    cors vol2

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  "http://meal-station-viktortap.vercel.app/",
-  "http://meal-station-git-main-viktortap.vercel.app/",
-  "http://meal-station.vercel.app/",
+  "http://meal-station-viktortap.vercel.app",
+  "http://meal-station-git-main-viktortap.vercel.app",
+  "http://meal-station.vercel.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
